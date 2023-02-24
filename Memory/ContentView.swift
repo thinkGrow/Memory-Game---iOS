125d07b2d0d4cae2088bc88dc5eefd931295c0bf
thinkGrow <rubaitreshad@gmail.com>
2023-02-24 22:48:39 +0600

    Added loops, struct etc

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -9,15 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack(content: {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text("👻")
-            })
-            .padding()
+        HStack{
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+        }
+            .padding(5)
             .foregroundColor(Color.orange)
             .font(Font.largeTitle)
+    }}
+
+struct CardView: View{
+    var isFaceUp: Bool
+    
+    var body: some View{
+        ZStack{
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👻")
         }
+    }
+    }
 }
 
 
