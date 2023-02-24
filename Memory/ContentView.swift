7b069b0d79d16397958d62ffc4a0c1931ae08c35
Rubait Reshad <rubaitreshad@gmail.com>
2023-02-24 17:49:59 +0600

    First Commit

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -9,15 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(content: {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            })
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
         }
-        .padding()
-    }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
