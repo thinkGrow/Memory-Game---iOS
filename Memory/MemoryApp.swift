03becc6f546e4f73eda5816fe97420978cba84d3
Rubait Reshad <rubaitreshad@gmail.com>
2023-02-24 11:38:07 +0600

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Rubait Reshad on 24/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
